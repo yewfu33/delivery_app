a07c150b833aea99bdc4a87c35920aa82e5171a9
yewfu33 <53574514+yewfu33@users.noreply.github.com>
2020-12-15 16:08:02 +0800

    fix laji documentation

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -21,10 +21,15 @@ class NotificationService {
     }
   }
 
+  Future getFcmToken() async {
+    return await _fcm.getToken();
+  }
+
   void configure() {
     _fcm.configure(
       // this callback is used when the app runs on the foreground
       onMessage: handleOnMessage,
+      onBackgroundMessage: myBackgroundMessageHandler,
       // used when the app is closed completely and is launched using the notification
       onLaunch: handleOnLaunch,
       // when its on the background and opened using the notification drawer
@@ -33,7 +38,7 @@ class NotificationService {
   }
 
   Future handleOnMessage(Map<String, dynamic> data) async {
-    print("=== data = ${data.toString()}");
+    print("=== data:onmessage = ${data.toString()}");
   }
 
   Future handleOnLaunch(Map<String, dynamic> data) async {
@@ -42,5 +47,15 @@ class NotificationService {
 
   Future handleOnResume(Map<String, dynamic> data) async {
     print("=== data = ${data.toString()}");
+  }
+
+  static Future myBackgroundMessageHandler(Map<String, dynamic> message) async {
+    print("myBackgroundMessageHandler message: $message");
+    int msgId = int.tryParse(message["data"]["msgId"].toString()) ?? 0;
+    print("msgId $msgId");
+    if (message.containsKey('notification')) {
+      // Handle notification message
+      print(message['notification']);
+    }
   }
 }
